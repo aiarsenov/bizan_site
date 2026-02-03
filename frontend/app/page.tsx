f271396638381fee99746ca2f6f2fb418a5bfafa
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2026-02-03 10:25:05 +0300

    Отображение header и footer

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,13 @@
-import Main from '@/components/Main/Main';
+import Header from "@/components/Header/Header";
+import Main from "@/components/Main/Main";
+import Footer from "@/components/Footer/Footer";
 
 export default async function Home() {
-  return <Main />;
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    );
 }

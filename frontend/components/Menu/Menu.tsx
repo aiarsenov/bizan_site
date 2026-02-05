6eae64f099987295c160592b3048fe3690fae5c8
Владислав Ухлин <heavybird@MacBook-Pro-Vladislav.local>
2026-02-05 17:12:01 +0300

    Переработка меню. Добавлен метод получения пунктов меню

--- frontend/components/Menu/Menu.tsx
+++ frontend/components/Menu/Menu.tsx
@@ -1,50 +1,87 @@
 "use client";
 
+import { useState, useEffect } from "react";
+
+import { getMenuList } from "@/lib/api";
+
 import "./Menu.scss";
 
-type MenuItem = {
+type MenuItemAttributes = {
+    title: string;
     url: string;
-    label: string;
+    order: number;
+    isVisible: boolean;
+};
+
+type MenuItem = {
+    id: number;
+    attributes: MenuItemAttributes;
 };
 
 const MENU_ITEMS: MenuItem[] = [
     {
-        url: "#services",
-        label: "Услуги",
+        id: 2,
+        attributes: {
+            title: "Услуги",
+            url: "#services",
+            order: 1,
+            isVisible: true,
+        },
     },
     {
-        url: "#projects",
-        label: "Проекты",
+        id: 3,
+        attributes: {
+            title: "Проекты",
+            url: "#projects",
+            order: 2,
+            isVisible: true,
+        },
     },
     {
-        url: "#about",
-        label: "О компании",
+        id: 7,
+        attributes: {
+            title: "Медиа",
+            url: "#media",
+            order: 4,
+            isVisible: true,
+        },
     },
     {
-        url: "#team",
-        label: "Команда",
-    },
-    {
-        url: "#media",
-        label: "Медиа",
-    },
-    {
-        url: "#contacts",
-        label: "Контакты",
+        id: 6,
+        attributes: {
+            title: "Команда",
+            url: "#team",
+            order: 3,
+            isVisible: true,
+        },
     },
 ];
 
 export default function Menu() {
+    const [data, setData] = useState(MENU_ITEMS);
+
+    useEffect(() => {
+        const loadData = async () => {
+            const response = await getMenuList();
+
+            if (response) {
+                setData(response);
+            }
+        };
+
+        loadData();
+    }, []);
+
     return (
         <nav className="menu">
-            {MENU_ITEMS.map((item, index) => (
+            {data.map((item) => (
                 <a
                     className="menu__item"
-                    href={item.url}
-                    aria-label={item.label}
-                    key={index}
+                    href={item.attributes.url}
+                    aria-label={item.attributes.title}
+                    key={item.id}
                 >
-                    {item.label}
+                    {item.attributes.title}
                 </a>
             ))}
         </nav>
